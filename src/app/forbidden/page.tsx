9291bd8de8fc5f1790af3c4e708f3291bd987301
Kristján Jakob Ásgrímsson <kristjanjakobasgrimsson@rfc1918.rhi.hi.is>
2026-04-03 20:55:44 +0000

    Bæta við forbidden skrá

---
+++ src/app/forbidden/page.tsx
@@ -0,0 +1,8 @@
+export default function ForbiddenPage() {
+    return (
+      <section>
+        <h1>Ekki heimild</h1>
+        <p>Þú hefur ekki heimild til að skoða þessa síðu.</p>
+      </section>
+    );
+}
